c7d85875b8a2e64ed4541236e77cb933622d69b0
Raghav-upes <ssrh7454@gmail.com>
2024-09-02 11:06:03 +0530

    Part3

---
+++ Assets/Character_Death_FX/Scripts/CharacterFire.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterFire : MonoBehaviour
+{
+
+    public Material[] originalMaterials;  // Store the original materials
+    private Material[] mat;  // Array of cloned materials
+    public GameObject fireFX;
+    public Light ExplodeLight;
+
+    private bool startFade = false;  // For letting the script know it's time to start fading out
+    private float t = 0.0f;
+    private float fadeStart = 4;
+    private float fadeEnd = 0;
+    private float fadeTime = 1;
+    private float pauseTime = 0;
+    private float fadeSpeed = 0.05f;
+
+    void FaceCamera(GameObject obj)
+    {
+        // Calculate the direction from the object to the camera
+        Vector3 directionToCamera = (Camera.main.transform.position - obj.transform.position).normalized;
+
+        // Adjust the direction so that it doesn't look upwards or downwards if you want a flat rotation
+        directionToCamera.y = 0;
+
+        // Rotate the object to face the camera
+        obj.transform.rotation = Quaternion.LookRotation(directionToCamera);
+    }
+
+    void Start()
+    {
+
+        // Clone the materials
+        mat = new Material[originalMaterials.Length];
+        for (int i = 0; i < originalMaterials.Length; i++)
+        {
+            mat[i] = new Material(originalMaterials[i]);  // Clone the material
+        }
+
+        // Assign the cloned materials to the game object
+        GetComponentInChildren<Renderer>().materials = mat;
+
+        // Reset all cloned materials to opaque untextured matte grey - good for testing purposes
+        for (int rend = 0; rend <= mat.Length - 1; rend++)  // For each material
+        {
+            mat[rend].SetOverrideTag("RenderType", "");
+            mat[rend].SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+            mat[rend].SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+            mat[rend].SetInt("_ZWrite", 1);
+            mat[rend].DisableKeyword("_ALPHATEST_ON");
+            mat[rend].DisableKeyword("_ALPHABLEND_ON");
+            mat[rend].DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat[rend].renderQueue = -1;
+        }
+
+        fireFX.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BlueBullet"))
+        {
+            this.GetComponent<CapsuleCollider>().enabled = false;
+            ResetMyMat();
+
+            // Fade out the explosion light
+            StartCoroutine("FadeLight");
+
+            // Fade out the character
+            StartCoroutine(FadeOut());
+
+            // Activate the fire effects
+            fireFX.SetActive(true);
+            this.gameObject.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        FaceCamera(this.gameObject);
+    }
+
+    private void ResetMyMat()
+    {
+        // Set all materials to Fade rendering mode so they can be faded out
+        for (int setfade = 0; setfade < mat.Length; setfade++)
+        {
+            mat[setfade].SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat[setfade].SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat[setfade].EnableKeyword("_ALPHABLEND_ON");
+            mat[setfade].DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat[setfade].SetInt("_ZWrite", 0);
+            mat[setfade].DisableKeyword("_ALPHATEST_ON");
+            mat[setfade].renderQueue = 3000;
+            mat[setfade].SetFloat("_Mode", 2);
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        bool isFading = true;
+        while (isFading)
+        {
+            isFading = false;
+
+            for (int rend = 0; rend <= mat.Length - 1; rend++)
+            {
+                Color color = mat[rend].color;  // The variable "color" is the renderer's material color
+                if (color.a > 0)
+                {  // If the color's alpha is greater than 0
+                    color.a -= fadeSpeed;  // Decrease the color's alpha by fadeSpeed
+                    if (color.a < 0) color.a = 0;  // Clamp alpha to zero
+                    mat[rend].color = color;  // Update the renderer's material color
+                    isFading = true;  // Continue the loop until all materials are fully faded
+                }
+            }
+
+            yield return null;  // Wait for the next frame before continuing
+        }
+
+        StartCoroutine(DestroyMe());
+    }
+
+    IEnumerator DestroyMe()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Destroy(gameObject);
+    }
+
+    IEnumerator FadeLight()
+    {
+
+        while (t < fadeTime)
+        {
+            if (pauseTime == 0)
+            {
+                t += Time.deltaTime;
+            }
+
+            ExplodeLight.intensity = Mathf.Lerp(fadeStart, fadeEnd, t / fadeTime);
+            yield return 0;
+        }
+
+        t = 0;
+    }
+}
