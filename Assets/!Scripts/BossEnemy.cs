41ce5fbd1a4726d6ec2154bdcf40f9d4dd59389e
Siddharth Verma <siddharthverma424@gmail.com>
2024-10-04 17:19:41 +0530

    audio

--- Assets/!Scripts/BossEnemy.cs
+++ Assets/!Scripts/BossEnemy.cs
@@ -167,7 +167,7 @@ public class BossEnemy : MonoBehaviour
     {
         yield return new WaitForSeconds(0.13f);
         CreateCrater(transform.position);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         
 
 
